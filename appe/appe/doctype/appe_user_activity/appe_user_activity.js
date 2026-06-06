b833840b2c2b182dfd8c38b967a134ccf3888ee2
root <root@AppeTech.io>
2026-06-06 15:40:32 +0530

    Now Appe User Activity Tracking Available

---
+++ appe/appe/doctype/appe_user_activity/appe_user_activity.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Appe technologies and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Appe User Activity", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ appe/appe/doctype/appe_user_activity/appe_user_activity.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Appe technologies and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class AppeUserActivity(Document):
+	pass
